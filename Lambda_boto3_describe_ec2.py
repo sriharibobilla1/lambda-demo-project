2d739a36df7cfe34af777080d120bc1a2837c142
sriharibobilla1 <srihari.bobilla@gmail.com>
2020-04-23 12:15:15 -0400

    Update Lambda_boto3_describe_ec2.py

--- Lambda_boto3_describe_ec2.py
+++ Lambda_boto3_describe_ec2.py
@@ -1,14 +1,23 @@
 import boto3 
 from sys import argv
 
+# Script will need 2 arg inputs while running
+
 argKey = '$1'
 argValue = '$2'
 
 ec2client = boto3.client('ec2')
+
+# This decsribes the EC2 Instances filtering with the tag:key/value
+
 response = ec2client.describe_instances(Filters=[{'Name' : 'argKey','Values' : ['argValue']}])
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
-        # This sample print will output entire Dictionary object
+        
+        # Prints output entire Dictionary object
+        
         print(instance)
-        # This will print will output the value of the Dictionary key 'InstanceId'
+        
+        # Prints output the value of the Dictionary key 'InstanceId'
+        
         print(instance["InstanceId"])
